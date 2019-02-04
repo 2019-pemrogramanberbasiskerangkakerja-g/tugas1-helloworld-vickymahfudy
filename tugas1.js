43454d966d688816f68ec1bd3284012e1f8eb14d
vickymahfudy <vicky15@mhs.if.its.ac.id>
2019-02-04 15:39:17 +0700

    Tugas 1 : Express Hello World

--- tugas1.js
+++ tugas1.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+var express = require('express');
+var app = express();
+
+app.listen(8000, function() {
+	console.log('Listening Port 8000');
+});
+
+app.get('/', function(request, response) {
+	response.send("Hello World!")
+});
